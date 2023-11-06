9f099a855e6eb01db1edb0c98d3df081316d53f1
Roma <r.nosovich@paralect.com>
2023-11-06 01:38:06 +0100

    returning some dummy data

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, { Express, Request, Response, Application } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 //For env File 
@@ -7,10 +8,15 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Alive!');
+    res.json([
+        { name: 'test', age: 15 },
+        { name: 'test2', age: 22 }
+    ])
 });
 
 app.listen(port, () => {
-  console.log(`Server is up and running at http://localhost:${port}`);
+    console.log(`Server is up and running at http://localhost:${port}`);
 });
